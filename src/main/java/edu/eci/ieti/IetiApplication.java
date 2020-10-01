15cb9d84352dc54ecadd672c9e15633e1ee95b7e
checho1998 <c.hecho.28@hotmail.com>
2020-10-01 13:04:57 -0500

    fin

--- src/main/java/edu/eci/ieti/IetiApplication.java
+++ src/main/java/edu/eci/ieti/IetiApplication.java
@@ -3,11 +3,25 @@ package edu.eci.ieti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class IetiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IetiApplication.class, args);
+		//SpringApplication.run(IetiApplication.class, args);
+		SpringApplication app = new SpringApplication(IetiApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+		app.run(args);
 	}
-
+	/**
+	 * Optiene puerto del entorno.
+	 * @return port del entorno
+	 */
+	static int getPort() {
+		if (System.getenv("PORT") != null) {
+			return Integer.parseInt(System.getenv("PORT"));
+		}
+		return 8080; //returns default port if heroku-port isn't set (i.e. on localhost)
+	}
 }
